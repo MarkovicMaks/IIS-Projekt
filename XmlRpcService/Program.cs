f4589492847ce385f071668e6db9ba3f2f99bd46
MarkovicMaks <markovic.maks@gmail.com>
2025-06-25 10:06:30 +0200

    Dodao sve projekte

---
+++ XmlRpcService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace XmlRpcService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
